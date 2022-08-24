d6c9b60b23db470cb04b259268b046ea73f4dbaa
Stephanie <smmackin12@gmail.com>
2022-08-24 18:46:12 -0400

    documented

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,15 +2,19 @@ const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
 
+// takes data in the form of JS and returns the keys as SQL in order to use this data to update a colName in db.   
+//** */
+
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  // cols takes the keys and maps them each into a colName in the db. 
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
-
+  
   return {
     setCols: cols.join(", "),
     values: Object.values(dataToUpdate),
